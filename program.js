3bf7541c4e15d84724e85dcd270aa3ae9c8dc89d
NiteshTiwari89 <tiwarinitesh667@gmail.com>
2024-01-11 10:43:30 +0530

    new

---
+++ program.js
@@ -0,0 +1,17 @@
+var num=30
+var num2=10
+var num3=20
+if(num>num2 && num>num3)
+    {  
+        console.log(num," is the greatest number") 
+    }
+else if(num2>num && num2>num3)
+    {
+        console.log(num2," is the greatest number")
+    }
+else
+    {
+        console.log(num3," is the greatest number")
+    }
+
+console.log("HELLO BROTHER")
